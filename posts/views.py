f0d2aea31cb1fd7e5d00718027dd24f39102a15d
STnobile <stnobile0@gmail.com>
2023-09-07 12:58:31 +0000

    add filters for posts and profiles

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -11,7 +13,29 @@ class PostList(generics.ListCreateAPIView):
     """
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        comments_count=Count('comment', distinct=True),
+        likes_count=Count('likes', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        # user feed
+        'owner__followed__owner__profile',
+        # user likes posts
+        'likes__owner__profile',
+        # user posts
+        'owner__profile',
+    ]
+    search_fields =[
+        'owner__username',
+        'title',
+    ]
+    ordering_fields = ['created_at', 'comments_count', 'likes_count']
+
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -21,6 +45,9 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve a post and edit or delete it if you own it.
     """
-    serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        comments_count=Count('comment', distinct=True),
+        likes_count=Count('likes', distinct=True)
+    ).order_by('-created_at')
+    serializer_class = PostSerializer
