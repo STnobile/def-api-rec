87a1b93686df318dd404e4ab04c89cfe8d95b694
STnobile <stnobile0@gmail.com>
2023-09-11 19:49:43 +0000

    added the default image

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../koala.c109266be9a4', blank=True
+        upload_to='images/', default='../barrels_kdy0fh'
     )
 
     class Meta:
