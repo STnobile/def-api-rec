6e7d2e521429b89fc59e90f22e5035a8bc5f5830
STnobile <stnobile0@gmail.com>
2023-11-05 12:01:16 +0000

    Add the corsheaders to INSTALLED_APP,

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -66,21 +66,8 @@ ALLOWED_HOSTS = [
    '8000-stnobile-defapirec-lx0zvx51m5g.ws-eu105.gitpod.io',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-    CORS_ORIGIN_WHITELIST = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-elif 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
-    ]
 
 CORS_ALLOW_ALL_HEADERS = True
-CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
@@ -125,6 +112,18 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+if 'CLIENT_ORIGIN' in os.environ:
+     CORS_ALLOWED_ORIGINS = [
+         os.environ.get('CLIENT_ORIGIN')
+    ]
+else:
+     CORS_ALLOWED_ORIGIN_REGEXES = [
+         r"^https://.*\.gitpod\.io$",
+    ]
+
+CORS_ALLOW_CREDENTIALS = True
+
+
 ROOT_URLCONF = 'drf_api.urls'
 
 TEMPLATES = [
@@ -149,17 +148,18 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    } if 'DEV' in os.environ else dj_database_url.parse(
-        os.environ.get('DATABASE_URL')
-    )
-}
-
-# DATABASES = {'default':dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+if 'DEV' in os.environ:
+    DATABASES = {
+         'default': {
+             'ENGINE': 'django.db.backends.sqlite3',
+             'NAME': BASE_DIR / 'db.sqlite3',
+         }
+    }
+else:
+    DATABASES = {
+         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+    print ( 'connected')
 
 
 # Password validation
